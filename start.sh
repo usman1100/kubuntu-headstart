d1618b1bbd7023bac47ebceba8cdbf1ce7329d08
usman1100 <usmanahmed1100@gmail.com>
2022-01-27 19:46:27 +0500

    init

---
+++ start.sh
@@ -0,0 +1,19 @@
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install aria2 bleachbit cheese cmatrix curl ffmpeg fish git gnome-disk-utility mongodb-org neofetch net-tools qbittorent python3-pip speedcrunch openjdk-8-jre speedtest-cli texlive-full wget vlc -y
+
+cd ~/Downloads
+# downloading chrome
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+# downloading vscode
+wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+
+# downloading anydesk
+wget "https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb"
+
+
+# configuring git
+git config --global user.email "usmanahmed1100@gmail.com"
+git config --global user.name "usman1100"
+
