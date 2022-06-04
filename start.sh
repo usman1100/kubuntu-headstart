f30096435134655624f20e516dbb8ef4ef973ad9
usman1100 <usmanahmed1100@gmail.com>
2022-06-04 14:24:16 +0500

    more packages

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 sudo apt update && sudo apt upgrade -y
 
-sudo apt install aria2 bleachbit cheese cmatrix curl ffmpeg fish git gnome-disk-utility mongodb-org neofetch net-tools qbittorent python3-pip speedcrunch openjdk-8-jre speedtest-cli texlive-full wget vlc -y
+sudo apt install aria2 bleachbit cheese cmatrix curl ffmpeg fish git gnome-disk-utility mongodb-org neofetch net-tools qbittorent python3-pip speedcrunch openjdk-8-jre speedtest-cli texlive-full wget vlc neovim -y
 
 cd ~/Downloads
 # downloading chrome
@@ -9,8 +9,8 @@ wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 # downloading vscode
 wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
-# downloading anydesk
-wget "https://download.anydesk.com/linux/anydesk_6.1.1-1_amd64.deb"
+sudo apt install ./google-chrome-stable_current_amd64.deb
+sudo apt install ./code*.deb
 
 
 # configuring git
